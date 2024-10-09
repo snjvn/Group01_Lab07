a6748183f67a2110bdbee906684c30c291e4bc93
Sanjeev <snjvnxyz@gmail.com>
2024-10-09 23:35:22 +0530

    RTS and CTS with PORTF pins instead of PORTC

--- main.c
+++ main.c
@@ -17,27 +17,23 @@ int main(void)
     INIT_GPIO_PORTF_REGISTERS();
 
     SYSCTL_RCGCUART_R |= 0x02; // enabling clock to UART module 0
-    SYSCTL_RCGCGPIO_R |= 0x26; // enabling clock to PORTB, C
+    SYSCTL_RCGCGPIO_R |= 0x22; // enabling clock to PORTF, B
 
     GPIO_PORTB_LOCK_R = 0x4C4F434B;     /* unlock commit register */
     GPIO_PORTB_CR_R = 0x03;             /* make PORTF configurable */
-    GPIO_PORTB_AFSEL_R = 0x03; // selecting A0, A1 for UART operations
+    GPIO_PORTB_AFSEL_R = 0x03; // selecting B0, B1 for UART operations
     GPIO_PORTB_PCTL_R = 0x11; // muxing B0 and B1 to Rx and Tx pins of UART0 module, respectively
     GPIO_PORTB_DIR_R = 0x02;
     GPIO_PORTB_PUR_R = 0x02;
 
-    GPIO_PORTC_LOCK_R = 0x4C4F434B;     /* unlock commit register */
-    GPIO_PORTC_CR_R = 0x03;             /* make PORTF configurable */
-    GPIO_PORTC_AFSEL_R = 0x30; // selecting A0, A1 for UART operations
-    GPIO_PORTC_PCTL_R = 0x880000; // muxing B0 and B1 to Rx and Tx pins of UART0 module, respectively
-    GPIO_PORTC_DIR_R = 0x10;
+
 
 //    UART1_CTL_R = 0x00;
     UART1_IBRD_R = 130;
     UART1_FBRD_R = 13;
     UART1_LCRH_R |= 0x72;
     UART1_CC_R = 0x00;
-    UART1_CTL_R |= 0xC081; // enabling UART1 in loopback
+    UART1_CTL_R |= 0xC001; // enabling UART1 in loopback
     uint8_t rx_reg = 0x00;
 
     while(1){
@@ -77,29 +73,30 @@ void INIT_GPIO_PORTF_REGISTERS(){
     GPIO_PORTF_LOCK_R = 0x4C4F434B;     /* unlock commit register */
     GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
     GPIO_PORTF_DEN_R = 0x1F;            /* set PORTF pins 4 : 0 pins */
-    GPIO_PORTF_DIR_R = 0x0E;            /*  */
+    GPIO_PORTF_DIR_R = 0x0D;            /*  */
+    GPIO_PORTF_AFSEL_R = 0x11;
+    GPIO_PORTF_PCTL_R = 0x11;
     GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
-
 
     NVIC_EN0_R = 0x40000000; // 30th bit controls PORTF
     GPIO_PORTF_IS_R = 0x00; // interrupt sensitivity - edge
     GPIO_PORTF_IEV_R = 0x00; // GPIO Interrupt triggered at negative edge from Pulled-Up Switch
-    GPIO_PORTF_IM_R = 0x11; // unmasking both switches
+    GPIO_PORTF_IM_R = 0x10; // unmasking both switches
 }
 
 void GPIOInterrupt(){
-    PORTF_Interrupt = GPIO_PORTF_RIS_R & 0x11; // read which switch caused the interrupt
+    PORTF_Interrupt = GPIO_PORTF_RIS_R & 0x10; // read which switch caused the interrupt
 
     // for debouncing
     NVIC_EN0_R = 0x00000000; // 30th bit controls PORTF
     GPIO_PORTF_IM_R = 0x00; // masking both switches
-    if (PORTF_Interrupt == 0x01){ // switch was pressed, reduce brightness
+//    if (PORTF_Interrupt == 0x01){ // switch was pressed, reduce brightness
+//        GPIO_PORTF_ICR_R = 0x11; // for edge-triggered interrupts, necessary to clear the interrupt status
+//        message = 0xF0;
+//    }
+    if (PORTF_Interrupt == 0x10){ // switch was pressed, reduce brightness
         GPIO_PORTF_ICR_R = 0x11; // for edge-triggered interrupts, necessary to clear the interrupt status
         message = 0xF0;
-    }
-    else if (PORTF_Interrupt == 0x10){ // switch was pressed, reduce brightness
-        GPIO_PORTF_ICR_R = 0x11; // for edge-triggered interrupts, necessary to clear the interrupt status
-        message = 0xAA;
     }
 
 }
