2a1678129908dfb673f81006589a0b1cd5004588
Sanjeev <snjvnxyz@gmail.com>
2024-10-09 23:57:37 +0530

    few changes to PORTF CTS, RTS- still not working

--- main.c
+++ main.c
@@ -73,10 +73,10 @@ void INIT_GPIO_PORTF_REGISTERS(){
     GPIO_PORTF_LOCK_R = 0x4C4F434B;     /* unlock commit register */
     GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
     GPIO_PORTF_DEN_R = 0x1F;            /* set PORTF pins 4 : 0 pins */
-    GPIO_PORTF_DIR_R = 0x0D;            /*  */
+    GPIO_PORTF_DIR_R = 0x0E;            /*  */
     GPIO_PORTF_AFSEL_R = 0x11;
     GPIO_PORTF_PCTL_R = 0x11;
-    GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
+    GPIO_PORTF_PUR_R = 0x10;            /* PORTF0 and PORTF4 are pulled up */
 
     NVIC_EN0_R = 0x40000000; // 30th bit controls PORTF
     GPIO_PORTF_IS_R = 0x00; // interrupt sensitivity - edge
